4fa71d3a012394ffce0792f9f8419f70aae5e36d
Sasha Ajin <77781886+sasha-ajin@users.noreply.github.com>
2021-04-29 11:08:16 +0300

    Update settings.py

--- barber_shop/settings.py
+++ barber_shop/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_9#i(yg3bqx9m)i()o-=5@9e@0bx&%nss5uipn7%799wdhisk='
+SECRET_KEY = 'Hello)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
